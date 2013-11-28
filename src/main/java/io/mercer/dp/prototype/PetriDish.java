e22a3e500d6cd118367ca401aa9e863283ac87d5
Cargo Kult <cargokultist@gmail.com>
2013-11-28 08:05:37 +0200

    cleanup

--- src/main/java/io/mercer/dp/prototype/PetriDish.java
+++ src/main/java/io/mercer/dp/prototype/PetriDish.java
@@ -1,20 +1,17 @@
 package io.mercer.dp.prototype;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 public class PetriDish {
-    private Map cellRegistry = Maps.newHashMap();
     private LinkedList<Cell> cells = Lists.newLinkedList();
 
     public void tick() throws CloneNotSupportedException {
         LinkedList<Cell> newCells = Lists.newLinkedList();
         for (Cell cell : cells) {
-            newCells.addLast(cell.multiply());
+            newCells.add(cell.multiply());
         }
         cells.addAll(newCells);
     }
@@ -24,6 +21,6 @@ public class PetriDish {
     }
 
     public void feed(Cell cell) {
-        cells.addLast(cell);
+        cells.add(cell);
     }
 }
